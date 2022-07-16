6ba874c87688cfc7cd9066f6a34c7df56d869656
Victor <66438596+Victor-Maznichenko@users.noreply.github.com>
2022-07-16 14:52:14 +0300

    connect fonts

---
+++ src/views/Catalog.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="catalog">
+    <div class="catalog__inner">
+      <h1 class="catalog__title title">ВЫБЕРИТЕ транспортное средство</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CatalogView'
+}
+</script>
