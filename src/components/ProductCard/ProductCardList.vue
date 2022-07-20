55ce85db5e1605993aaeca2ac780c9b21706c372
Victor <66438596+Victor-Maznichenko@users.noreply.github.com>
2022-07-20 09:09:56 +0300

    sdfsdfsdf

--- src/components/ProductCard/ProductCardList.vue
+++ src/components/ProductCard/ProductCardList.vue
@@ -1,17 +1,14 @@
 <template>
     <div class="catalog__items-list">
-        <ProductCard class="catalog__item" v-for="item in productCards" :key="item.id" :productCard="item" />
+        <!-- <ProductCard class="catalog__item" v-for="item in productCards" :key="item.id" :productCard="item" /> -->
     </div>
 </template>
 
 <script>
-import ProductCard from '@/components/ProductCard/ProductCard.vue'
+// import ProductCard from '@/components/ProductCard/ProductCard.vue'
 
 export default {
     name: 'ProductCardList',
-    components: {
-        ProductCard
-    },
     props: {
         productCards: {
             type: Object
